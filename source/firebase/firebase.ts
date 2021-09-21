c840dd21229e88e972bdc6925225aa08504a75c4
horvathaa <ambear9@gmail.com>
2021-09-21 14:00:53 -0400

    connect db and user login

--- source/firebase/firebase.ts
+++ source/firebase/firebase.ts
@@ -3,8 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-// AMBER: message me for this data
-  
+    apiKey: "AIzaSyDDs8lNI6hgbu-Y6Jjhx0RETyk3-gd_060",
+    authDomain: "adamite-b86e5.firebaseapp.com",
+    projectId: "adamite-b86e5",
+    storageBucket: "adamite-b86e5.appspot.com",
+    messagingSenderId: "644799342321",
+    appId: "1:644799342321:web:2b16d43ddedf59d69dab50",
+    measurementId: "G-SVJ90HQ6Z9"
 };
 
 firebase.initializeApp(config);
--- source/view/app/index.tsx
+++ source/view/app/index.tsx
@@ -8,6 +8,7 @@ declare global {
     acquireVsCodeApi(): any;
     data: Annotation[];
     selection: string;
+    login: boolean;
     addEventListener(): any;
   }
 }
@@ -17,15 +18,37 @@ const vscode = window.acquireVsCodeApi();
 window.addEventListener('message', event => {
   const message = event.data;
   switch(message.command) {
+    case 'login':
+      ReactDOM.render(
+        <AdamitePanel 
+          vscode={vscode} 
+          data={[]} 
+          selection={""} 
+          login={true}
+        />, 
+        document.getElementById('root')
+      );
+      return;
     case 'update': 
       ReactDOM.render(
-        <AdamitePanel vscode={vscode} data={message.payload.annotationList} selection={""} />, 
+        <AdamitePanel 
+          vscode={vscode} 
+          data={message.payload.annotationList} 
+          selection={""} 
+          login={false}
+        />, 
         document.getElementById('root')
       );
       return;
     case 'newAnno':
+      console.log('in new anno')
       ReactDOM.render(
-        <AdamitePanel vscode={vscode} data={message.payload.annotationList} selection={message.payload.selection} />, 
+        <AdamitePanel 
+          vscode={vscode} 
+          data={message.payload.annotationList} 
+          selection={message.payload.selection} 
+          login={false}
+        />, 
         document.getElementById('root')
       );
       return;
@@ -36,6 +59,6 @@ window.addEventListener('message', event => {
 })
 
 ReactDOM.render(
-  <AdamitePanel vscode={vscode} data={window.data} selection={""} />,
+  <AdamitePanel vscode={vscode} data={window.data} selection={""} login={false} />,
   document.getElementById("root")
 );

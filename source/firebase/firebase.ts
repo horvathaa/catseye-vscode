6d290298a7ed5f5d38cb85489dc03bbfe90ef794
horvathaa <ambear9@gmail.com>
2021-10-26 13:50:09 -0400

    update to use dotenv for firebase API and add back in .vscode to simplify launch

--- source/firebase/firebase.ts
+++ source/firebase/firebase.ts
@@ -1,8 +1,19 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-// @ts-ignore
-import { config } from './secrets.app.js';
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(__dirname, '..\\..\\.env.local')});
+
+const config = {
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+    appId: process.env.FB_APP_ID,
+    measurementId: process.env.FB_MEASUREMENT_ID 
+}
 
 firebase.initializeApp(config);
 // firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
